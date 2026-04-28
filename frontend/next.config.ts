6ace70d5414b13a35b69bd1f17ed2f346f43bdea
henriqueCosta-Geo <henrique.costa@geocarbonite.com>
2026-04-28 15:23:12 -0300

    feat : relatórios integrados

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   productionBrowserSourceMaps: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
